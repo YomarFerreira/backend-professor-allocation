3106de75640067fdbf09b3632335974c9a1c1c85
YomarFerreira <yomar@ymail.com>
2021-10-02 13:04:41 -0300

    Project addict

---
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationApplication.class, args);
+	}
+
+}
