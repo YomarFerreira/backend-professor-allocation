91fea57441c83d6b80d979a5053fddbeecb73757
YomarFerreira <yomar@ymail.com>
2021-12-17 12:57:58 -0300

    removing unnecessary comments and files

--- src/main/java/com/project/professor/allocation/ProfessorAllocationApplication.java
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationApplication.java
@@ -8,12 +8,7 @@ public class ProfessorAllocationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProfessorAllocationApplication.class, args);
-		
-		//Comentário teste para fazer um "commit" no gitHub
-		//Comentário teste para fazer o 2º "commit" no gitHub
-		//Comentário teste para fazer o 3° "commit" no gitHub
-		//Comentário teste para fazer o 4º "commit" no gitHub
-		
+				
 	}
 
 }
